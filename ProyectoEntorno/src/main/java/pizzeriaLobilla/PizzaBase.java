68bd7406a7d4657acc3522ca5b2e9bacbbab9da3
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-06-03 10:34:31 +0200

    Creación de las diferentes variables que se utilizarán y sus respectivas explicacíones de cada una

--- ProyectoEntorno/src/main/java/pizzeriaLobilla/PizzaBase.java
+++ ProyectoEntorno/src/main/java/pizzeriaLobilla/PizzaBase.java
@@ -5,10 +5,29 @@
  */
 package pizzeriaLobilla;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Isaac
  */
 public class PizzaBase {
-    
+
+    /*Variable que controla  los ingredientes máximos 
+    que se puede añadir a una pizza, hay 12 puesto 
+    que se pueden añadir 1o mas los 2 que vienen ya por defecto*/
+         private int control = 12;
+    /*Variable del precio de pizza sin ingredientes añadidos*/
+         private double precio = 4;
+    /*Variable con el precio de cada ingrediente extra*/
+         private double precioExtra = 0.5;
+    /*ArrayList donde guardaremos los diferentes ingredientes a añadir*/
+         private ArrayList<String> ingredientes = new ArrayList<>();
+    /*Variable para saber la cantidad de ingredientes*/
+         private int cantidadIngredientes = 1;
+    /*Variable con los precios finales de las pizzas, tiene valor 0 puesto 
+    que se modificara cuando se les añada ingredientes o se eliga otra pizza*/
+         private double precioTotal = 0;
+    /*Variable String qu nos permitirá enviar un mensaje al usuario*/     
+         private String mensaje = "";
 }
