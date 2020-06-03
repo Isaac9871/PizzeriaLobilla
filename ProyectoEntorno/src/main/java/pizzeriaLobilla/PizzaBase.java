03df30ac69b6085fcc4647527f12c2eb216841c8
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-06-03 10:46:08 +0200

    Creación de constructor de la pizza base con sus ingredientes predeterminados

--- ProyectoEntorno/src/main/java/pizzeriaLobilla/PizzaBase.java
+++ ProyectoEntorno/src/main/java/pizzeriaLobilla/PizzaBase.java
@@ -16,18 +16,24 @@ public class PizzaBase {
     /*Variable que controla  los ingredientes máximos 
     que se puede añadir a una pizza, hay 12 puesto 
     que se pueden añadir 1o mas los 2 que vienen ya por defecto*/
-         private int control = 12;
+    private int control = 12;
     /*Variable del precio de pizza sin ingredientes añadidos*/
-         private double precio = 4;
+    private double precio = 4;
     /*Variable con el precio de cada ingrediente extra*/
-         private double precioExtra = 0.5;
+    private double precioExtra = 0.5;
     /*ArrayList donde guardaremos los diferentes ingredientes a añadir*/
-         private ArrayList<String> ingredientes = new ArrayList<>();
+    private ArrayList<String> ingredientes = new ArrayList<>();
     /*Variable para saber la cantidad de ingredientes*/
-         private int cantidadIngredientes = 1;
+    private int cantidadIngredientes = 1;
     /*Variable con los precios finales de las pizzas, tiene valor 0 puesto 
     que se modificara cuando se les añada ingredientes o se eliga otra pizza*/
-         private double precioTotal = 0;
-    /*Variable String qu nos permitirá enviar un mensaje al usuario*/     
-         private String mensaje = "";
+    private double precioTotal = 0;
+    /*Variable String qu nos permitirá enviar un mensaje al usuario*/
+    private String mensaje = "";
+
+    //Constructor con los ingredientes predeterminados
+    public PizzaBase() {
+        ingredientes.add("Tomate");
+        ingredientes.add("Mozzarela");
+    }
 }
