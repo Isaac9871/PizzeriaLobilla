7a2fa1c0a2f63e0d355134572235356f50157ea3
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-06-03 11:33:51 +0200

    Convertir la clase (PizzaLobilla) en herencia de la clase (PizzaBase), ajustar el precio de la pizza y añadir los ingredientes de dicha pizza,sin la posibilidad de añadir más ingredientes

--- ProyectoEntorno/src/main/java/pizzeriaLobilla/PizzaLobilla.java
+++ ProyectoEntorno/src/main/java/pizzeriaLobilla/PizzaLobilla.java
@@ -9,6 +9,21 @@ package pizzeriaLobilla;
  *
  * @author Isaac
  */
-public class PizzaLobilla {
+
+//Herencia de la clase PizzaBase,pudiendo acceder a todo el contenido de este
+public class PizzaLobilla extends PizzaBase{
+
+    public PizzaLobilla() {
+        /*Precio incial de la pizzaLobilla,introducimos 5 
+        para que el precio sea de 7,5 que su precio predeterminado*/
+        setPrecio(5);
+        //Ingredientes que tiene la pizza aparte de los ingredientes que tiene de por si la pizza Base
+        //Aqui no hay que añadir más ingredientes ,sino que se queda con los que tiene ya
+        super.getIngredientes().add("Atun");
+        super.getIngredientes().add("Cebolla");
+        
+    }
+     
+     
     
 }
