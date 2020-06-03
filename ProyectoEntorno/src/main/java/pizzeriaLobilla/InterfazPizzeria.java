05e798cb5d5faf830885c3009593446af9f9af92
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-06-03 12:02:58 +0200

    Creación de las variables y creación de las distintas relaciones de la clase con las otras clases

--- ProyectoEntorno/src/main/java/pizzeriaLobilla/InterfazPizzeria.java
+++ ProyectoEntorno/src/main/java/pizzeriaLobilla/InterfazPizzeria.java
@@ -5,6 +5,8 @@
  */
 package pizzeriaLobilla;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Isaac
@@ -12,7 +14,17 @@ package pizzeriaLobilla;
 public class InterfazPizzeria {
     //Método main
     public static void main(String[] args) {
-        
+            //Scanner, para añadir datos desde el teclado
+            Scanner teclado = new Scanner(System.in);
+            /*Variables necesarias (opcion) para elegir entre las diferentes pizzas,
+            (ingre)para opcion de añadir ingredientes y ingredientes para añadir los diferentes ingredientes*/
+             int ingre, opcion, ingredientes;
+             /*Relacionar esta clase con las otras 3 clases creadas antes,
+             para tener acceso a sus contenidos
+             */
+        PizzaBase base = new PizzaBase();
+        PizzaPericolosa peri = new PizzaPericolosa();
+        PizzaLobilla lobi = new PizzaLobilla();
     }
     
 }
